51f9de0794dfe4344c7d6cb5ad9f5f270ec6338a
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2022-09-19 15:18:44 -0300

    feat: adiciona o arquivo da pagina de configurações

---
+++ src/modules/Config/index.tsx
@@ -0,0 +1,11 @@
+import { Display } from "@components/common/layouts/Display";
+
+function ConfigPage() {
+  return (
+    <Display flex justify="center">
+      <p>Página não encontrada</p>
+    </Display>
+  );
+}
+
+export { ConfigPage };
